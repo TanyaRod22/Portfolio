abb39e2eade3d169490096f0378d51257961b471
tanya <tanyarodrigues067@gmail.com>
2025-08-07 09:51:16 -0400

    Refactor Contact component to include form handling and integrate email functionality; update Earth and Stars components to use Canvas for 3D rendering; remove Feedbacks component from App; enhance SectionWrapper for better layout management.

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -11,7 +11,7 @@ const SectionWrapper = (Component, idName) => function HOC() {
       viewport={{ once: true, amour:0.25 }}
       className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
     >
-      <span className="hash-span" id={idName}>&nbsp;</span>
+      <span className="hash-span" id={idName}></span>
       <Component />
     </motion.section>
   )
