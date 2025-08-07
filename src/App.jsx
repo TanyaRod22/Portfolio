a2d75c008f1742e6fd2592dd2689436ce32ed27f
tanya <tanyarodrigues067@gmail.com>
2025-08-07 10:34:19 -0400

    Resolving Error regaring js and jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from "./components";
+import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech, Works } from "./components";
 const App = () => {
   return (
     <BrowserRouter>
